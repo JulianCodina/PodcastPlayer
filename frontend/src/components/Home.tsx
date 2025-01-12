d78ce7f78d63c2dfc7dc6dc684c6d176718ad186
JulianCodina <depedrojulianismael@gmail.com>
2025-01-11 21:43:57 -0300

    me olvide de quitar algo xd

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -68,8 +68,6 @@ export default function Home({ busqueda }: Props) {
     <main>
       {busqueda === "" ? (
         <>
-          <LoadingSection />
-          <LoadingSection />
           <SeccionBox
             img="assets/repetir.png"
             texto1={user.name}
